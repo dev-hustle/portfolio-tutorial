6c9d5e559dbc8f7287645c4fbd87781228f9396a
dev-hustle <dev.hustle2@gmail.com>
2020-09-09 13:19:34 +0200

    Updates

---
+++ precache-manifest.ba949598a8a0056642fb109a3c221fc0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f619fe5ea9aaf4d40d0db772d6cefc",
+    "url": "/portfolio-tutorial/index.html"
+  },
+  {
+    "revision": "5b441719ef0772cd74ba",
+    "url": "/portfolio-tutorial/static/css/main.7c1f462d.chunk.css"
+  },
+  {
+    "revision": "2feed184ddc7c8fdbfef",
+    "url": "/portfolio-tutorial/static/js/2.8ee5b095.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-tutorial/static/js/2.8ee5b095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b441719ef0772cd74ba",
+    "url": "/portfolio-tutorial/static/js/main.90f702eb.chunk.js"
+  },
+  {
+    "revision": "5033442992e810cd3fe5",
+    "url": "/portfolio-tutorial/static/js/runtime-main.be31536d.js"
+  },
+  {
+    "revision": "87b0dd6159ec9cc59d1c69db910e3af8",
+    "url": "/portfolio-tutorial/static/media/bg-contact-page.87b0dd61.svg"
+  },
+  {
+    "revision": "4f4919a305cd0c921b1a6405eeb2449b",
+    "url": "/portfolio-tutorial/static/media/bg-mobile-projects-page.4f4919a3.svg"
+  },
+  {
+    "revision": "d0e5775d2b3ee58d2da7cd582d2e2e44",
+    "url": "/portfolio-tutorial/static/media/bg-mobile-skills-page.d0e5775d.svg"
+  },
+  {
+    "revision": "182996c22c6e50f82b5842f1106cb4aa",
+    "url": "/portfolio-tutorial/static/media/bg-projects-page.182996c2.svg"
+  },
+  {
+    "revision": "e5852b8c80c42e1654ff87428181b85b",
+    "url": "/portfolio-tutorial/static/media/bg-skills-page.e5852b8c.svg"
+  },
+  {
+    "revision": "640db481736ff876d53dc029b5b9a18e",
+    "url": "/portfolio-tutorial/static/media/sample_1.640db481.png"
+  },
+  {
+    "revision": "1c0be871c1fd35f487906e8acc1cba4a",
+    "url": "/portfolio-tutorial/static/media/sample_2.1c0be871.png"
+  }
+]);
